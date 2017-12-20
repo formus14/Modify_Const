a089e063bbcec43ef57247e3598cf88b9d2b4cda
Omar Hussein <formus14@users.noreply.github.com>
2017-12-20 10:43:52 +0100

    Create modify_const_with_volatile_function.cpp

---
+++ modify_const_with_volatile_function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void IChangeConst(int* pa) {
+*pa = 5;
+cout << "IChangeConst: *pa : " << *pa << endl;
+}
+
+
+
+int main() {
+volatile const int a = 22;
+
+int *ptr = (int*)&a;
+IChangeConst(ptr);
+cout << "Value of a after IChangeConst : " << a << endl;
+
+volatile const int &b = a;
+cout << "Value of b which is reference to a :" << b << endl;
+
+int c = a;
+cout << "Valu of C - equal to value of a : " << c << endl;
+cin.get();
+return 0;
+
+}
