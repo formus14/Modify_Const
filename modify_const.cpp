0fe6470e334e3cbd1f1affd00b851a904486bd94
Omar Hussein <formus14@users.noreply.github.com>
2017-12-20 10:12:01 +0100

    Create modify_const.cpp
    credits to Ashish Sarode

---
+++ modify_const.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void IChangeConst(int* pa) {
+*pa = 5;
+cout << "IChangeConst: *pa : " << *pa << endl;
+}
+
+int main() {
+
+const int a = 22;
+int *ptr = (int*)&a;
+
+IChangeConst(ptr);
+cout << "Value of a after IChangeConst : " << a << endl;
+
+const int &b = a;
+cout << "Value of b which is reference to a :" << b << endl;
+
+int c = a;
+cout << "Valu of C - equal to value of a : " << c << endl;
+system("pause");
+return 0;
+}
